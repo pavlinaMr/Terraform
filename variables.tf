51b4e3b4a6d724e782746cc2931414b456c09085
pavlinaMr <114737614+pavlinaMr@users.noreply.github.com>
2022-10-07 16:17:34 +0300

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,41 +1,54 @@
 variable "region" {
-  type = string 
-  default = "us-east-1"
+    type = string
 }
 
-variable "namespace" {
-    description = "The project namespace to use for unique resource naming"
-    type = string
-    default = "pavlina_prod_byoi"
-}
 
 variable "availability_zone_A" {
     type = string
-    default = "us-east-1a"
 }
 
 variable "availability_zone_B" {
     type = string
-    default = "us-east-1b"
 }
 
-variable "vpc_cidr" {
+variable "taggings" {
     type = map(string)
 
     default = {
-        pavlina_prod_byoi = "10.0.0.0/16"
+        customer    =   "pavlina"
+        environment =   "prod"
+        solution    =   "byoi"
     }
+} 
+
+variable "namespace" {
 }
 
-variable "subnets" {
-    type = map(string)
+variable "db_admin_username" {
+    default = "admin"
+}
 
-    default = {
-        pavlina_prod_byoi_public_subnet_1      = "10.0.10.0/28"
-        pavlina_prod_byoi_public_subnet_2      = "10.0.11.0/28"
-        pavlina_prod_byoi_private_subnet_db_1  = "10.0.12.0/28"
-        pavlina_prod_byoi_private_subnet_db_2  = "10.0.13.0/28"
-        pavlina_prod_byoi_private_subnet_app_1 = "10.0.14.0/28"
-        pavlina_prod_byoi_private_subnet_app_2 = "10.0.15.0/28"
-    }
+variable "db_admin_password" {
+    default = "pm14dm1n!"
+    sensitive = true
+}
+
+variable "alb_sg" {
+}
+
+variable "alb_tg_arn" {
+}
+variable "private_subnet_app1" {
+}
+variable "private_subnet_app2" {
+}
+
+variable "vpc_id" {
+}
+
+variable "private_subnet_db1" {
+}
+variable "private_subnet_db2" {
+}
+variable "application_profile" {
 }
